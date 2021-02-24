df50ea5c605fbf91950314707f7cd924748af8ca
Michael Lan <michaellan202@gmail.com>
2021-02-24 14:11:31 -0800

    move server responsibility to different file

---
+++ src/matrix_ls_proto/server.clj
@@ -0,0 +1,6 @@
+(ns matrix-ls-proto.server)
+
+;; NOTE: This file defines some server variables for use by other files
+
+(def server "http://localhost:8008/_matrix")
+(def https-server "https://localhost:8448/_matrix")
