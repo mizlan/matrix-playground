480e29fffb26ff06f679b3757d1a6e7a06de6ed8
Michael Lan <michaellan202@gmail.com>
2021-02-24 21:30:18 -0800

    Tests for core

--- test/matrix_ls_proto/core_test.clj
+++ test/matrix_ls_proto/core_test.clj
@@ -1,7 +1,3 @@
 (ns matrix-ls-proto.core-test
   (:require [clojure.test :refer :all]
-            [matrix-ls-proto.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 0))))
+            [matrix-ls-proto.core :as sut]))
