10430ecdc0e1f99a8ee52dcdb15d1a3856494485
Michael Lan <michaellan202@gmail.com>
2021-02-24 21:29:32 -0800

    use "sut" for "system under test"

--- test/matrix_ls_proto/register_test.clj
+++ test/matrix_ls_proto/register_test.clj
@@ -1,8 +1,8 @@
 (ns matrix-ls-proto.register-test
-  (:require [matrix-ls-proto.register :as reg]
+  (:require [matrix-ls-proto.register :as sut]
             [matrix-ls-proto.core :as core]
             [clojure.test :refer :all]))
 
 (deftest access-test
   (testing "Get an access token"
-    (is (some? (reg/process-register)))))
+    (is (some? (sut/process-register)))))
