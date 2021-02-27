976814901006b6d27c87646c7ea27d768507cda1
Michael Lan <michaellan202@gmail.com>
2021-02-26 16:12:16 -0800

    Rename + Refactor

--- src/matrix_ls_proto/rooms.clj
+++ src/matrix_ls_proto/rooms.clj
@@ -6,18 +6,20 @@
 
 (def room-ext "/client/r0/createRoom")
 
-(defn create-room
+(defn query-create-room
   [server room-name access-token]
   (let [link (str server room-ext)
         form-params {"room_alias_name" room-name}
-        query-params {"access_token" access-token}]
-    (parse-string (:body (client/post link
-                                      {:form-params form-params
-                                       :query-params query-params
-                                       :accept :json
-                                       :content-type :json})))))
+        query-params {"access_token" access-token}
+        opts {:form-params form-params
+              :query-params query-params
+              :accept :json
+              :content-type :json
+              :throw-exceptions? false}]
+    (parse-string (:body (client/post link opts)))))
 
-(defn test-create-room
-  []
-  (let [access-token (reg/process-register)]
-    (create-room server "clj-room" access-token)))
+
+  (defn test-create-room
+    []
+    (let [access-token (reg/process-register)]
+      (query-create-room server "clj-room" access-token)))
