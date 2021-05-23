fa4da6cc2cfecfb4962511ab23c8ddc727314d88
Michael Lan <michaellan202@gmail.com>
2021-05-22 22:06:22 -0700

    Add other driver code

--- src/matrix_ls_proto/core.clj
+++ src/matrix_ls_proto/core.clj
@@ -25,3 +25,42 @@
       :body
       json/parse-string
       (get "access_token")))
+
+(defn format-message [msg]
+  (format "[%s]: %s"
+          (get-display-name (get msg "sender"))
+          (get-in msg ["content" "body"])))
+
+(def get-display-name
+  (memoize
+   (fn [user-id]
+     (-> (client/get (str *server* "/client/r0/profile/" user-id "/displayname"))
+         :body
+         (json/parse-string keyword)
+         :displayname))))
+
+;; (defn all-events
+
+(comment
+  (p/open)
+  (p/clear)
+  (p/close)
+  (add-tap #'p/submit)
+  (def a (get-access-token))
+  (def info (sync/attempt-sync *server* a))
+  (def tl (-> info
+              (get-in ["rooms" "join"])
+              only
+              (get-in ["timeline" "events"])))
+  (def msgs (filter #(= "m.room.message" (get-in % ["type"])) tl))
+  (def msg-texts (map format-message msgs))
+  (tap> msg-texts)
+  (tap> info)
+  (def nb (info "next_batch"))
+  (def nnb (next-info "next_batch"))
+  (tap> nb)
+  (def next-info (sync/attempt-sync *server* a nb))
+  (def next-next-info (sync/attempt-sync *server* a nnb))
+  (tap> next-info)
+  (tap> next-next-info)
+  )
