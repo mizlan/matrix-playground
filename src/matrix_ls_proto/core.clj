345c97c716398895992e7a17145a42e65c6f6f30
Michael Lan <michaellan202@gmail.com>
2021-05-22 22:05:53 -0700

    Add basic structure for driver code

--- src/matrix_ls_proto/core.clj
+++ src/matrix_ls_proto/core.clj
@@ -1,6 +1,22 @@
 (ns matrix-ls-proto.core
-  (:gen-class))
+  (:gen-class)
+  (:require [matrix-ls-proto.server :as s]
+            [matrix-ls-proto.login :as login]
+            [matrix-ls-proto.sync :as sync]
+            [cheshire.core :as json]
+            [portal.api :as p]
+            [clj-http.client :as client]
+            [matrix-ls-proto.secrets :as secrets]))
 
 (defn -main
   [& args]
   (println "Hello, World!"))
+
+(def ^:dynamic *server* "https://matrix.org/_matrix")
+
+(defn get-access-token []
+  (-> (login/attempt-login *server* {:username "mizlan_t"
+                                     :password secrets/pw})
+      :body
+      json/parse-string
+      (get "access_token")))
