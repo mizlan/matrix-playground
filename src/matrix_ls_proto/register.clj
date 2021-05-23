385b9f89d6c4f41979ef372389e2447a8fc3f485
Michael Lan <michaellan202@gmail.com>
2021-05-22 22:02:39 -0700

    Refactor register code

--- src/matrix_ls_proto/register.clj
+++ src/matrix_ls_proto/register.clj
@@ -1,15 +1,15 @@
 (ns matrix-ls-proto.register
   (:require [clj-http.client :as client]
-            [cheshire.core :refer :all]
+            [cheshire.core :as json]
             [matrix-ls-proto.server :refer [http-server]]))
 
 (def reg-ext "/client/r0/register")
 
-(defn attempt-register
+(defn query-register
   "Given a server and username+password combination, attempt to register."
   [server {:keys [username password]}]
   (let [link (str server reg-ext)]
-    (parse-string
+    (json/parse-string
      (:body (client/post link
                          {:insecure? false
                           :form-params
@@ -23,7 +23,7 @@
 (defn test-register
   "Use the HTTP server"
   []
-  (attempt-register http-server {:username "justtesting" :password "superdupergood"}))
+  (query-register http-server {:username "justtesting" :password "superdupergood"}))
 
 ;; TODO: use a tuple instead of printing to stdout
 (defn process-register
